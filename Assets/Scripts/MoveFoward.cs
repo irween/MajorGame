664003f6e6f6bf3125cf895026663cfdd424ed3f
Irwan Clark <cben4441@gmail.com>
2022-09-13 07:53:20 +1200

    fixed gun  shooting
    Gun now shoots straight rather than at an angle.

--- Assets/Scripts/MoveFoward.cs
+++ Assets/Scripts/MoveFoward.cs
@@ -6,19 +6,17 @@ public class MoveFoward : MonoBehaviour
 {
     // public variables
     public float speed = 40f;
-    public Vector3 offset  = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        // changes the rotation of the object
-        transform.Rotate(offset);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // moves the gameobject by a designated amount every frame
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
     }
 }
